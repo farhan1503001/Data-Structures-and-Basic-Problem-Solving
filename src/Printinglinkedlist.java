2b44b20e70854dacdd51769fd31e163309eac3ee
Mohd.Farhan Israk Soumik <farhanisrak1503001@gmail.com>
2021-07-14 08:58:51 +0600

    linkedlistdone

--- src/Printinglinkedlist.java
+++ src/Printinglinkedlist.java
@@ -12,7 +12,46 @@ class SinglyLinkedListNode {
 	}
 }
 public class Printinglinkedlist {
-
+	public static void print_reverse(SinglyLinkedListNode head)
+	{
+		if(head==null)
+		{
+			return;
+		}
+		print_reverse(head.next);
+		System.out.print(head.data+" ");
+	}
+	public static SinglyLinkedListNode reverse_list(SinglyLinkedListNode head) {
+		   if(head==null)
+		   {
+			   return null;
+		   }
+		   else if(head.next==null)
+		   {
+			   return head;
+		   }
+		   else {
+			   SinglyLinkedListNode curr=head;
+			   SinglyLinkedListNode prev=null;
+			   System.out.println("Reverse Linked list test \n");
+			   while(curr!=null)
+			   {  
+				   SinglyLinkedListNode temp=prev;
+				   /*
+				   if(temp!=null) {
+					   System.out.print(temp.data+" ");
+				   }
+				   */
+				   prev=curr;
+				   //System.out.print(prev.data+" ");
+				   curr=curr.next;
+				   prev.next=temp;
+			   }
+			   System.out.println(prev.data);
+			   System.out.println(prev.next.data);
+			   return prev;
+		   }
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           Scanner sc=new Scanner(System.in);
@@ -40,6 +79,13 @@ public class Printinglinkedlist {
           SinglyLinkedListNode n_head=insert_position(nhead,post,50);
           display_list(n_head);
           //display_list(n_head);
+          n_head=deleteposition(n_head,0);
+          display_list(n_head);
+          System.out.println();
+          print_reverse(n_head);
+          System.out.println("Reversed Linked List");
+          SinglyLinkedListNode newhead=reverse_list(n_head);
+          display_list(newhead);
           sc.close();
 	}
 	public static void display_list(SinglyLinkedListNode head)
@@ -101,8 +147,30 @@ public class Printinglinkedlist {
 		}
 		
 	}
-	public static void deleteposition(SinglyLinkedListNode head,int position)
+	public static SinglyLinkedListNode deleteposition(SinglyLinkedListNode head,int position)
 	{
+		SinglyLinkedListNode curr=head;
+		if(curr.next==null)
+		{
+			return null;
+		}
+		else if(position==0)
+		{
+			SinglyLinkedListNode temp=curr.next;
+			curr.next=null;
+			curr=temp;
+			return curr;
+		}
+		else {
+			for(int i=0;i<position-1;i++)
+			{
+				curr=curr.next;
+			}
+			SinglyLinkedListNode temp=curr.next.next;
+			curr.next=null;
+			curr.next=temp;
+			return head;
+		}
 		
 	}
 
